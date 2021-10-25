5404d828c1b28f097a33dffece3b34eadee8deba
wuyachi <wuyachi@163.com>
2021-10-25 15:25:29 +0800

    merge

--- common/chains.go
+++ common/chains.go
@@ -280,25 +280,241 @@ type ProxyBalance struct {
 	ItemProxy string
 }
 
-func GetProxyBalance(chainId uint64, hash string, proxy string) (*big.Int, error) {
+func GetAllLockProxyBalance(chainId uint64, hash string) []*ProxyBalance {
+	proxyBalances := make([]*ProxyBalance, 0)
 	if chainId == basedef.ETHEREUM_CROSSCHAIN_ID {
-		return ethereumSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.MATIC_CROSSCHAIN_ID {
-		return maticSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.BSC_CROSSCHAIN_ID {
-		return bscSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.HECO_CROSSCHAIN_ID {
-		return hecoSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.OK_CROSSCHAIN_ID {
-		return okSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.NEO_CROSSCHAIN_ID {
-		return neoSdk.Nep5Balance(hash, proxy)
-	} else if chainId == basedef.ONT_CROSSCHAIN_ID {
-		return ontologySdk.Oep4Balance(hash, proxy)
-	} else if chainId == basedef.ARBITRUM_CROSSCHAIN_ID {
-		return arbitrumSdk.Erc20Balance(hash, proxy)
-	} else if chainId == basedef.XDAI_CROSSCHAIN_ID {
-		return xdaiSdk.Erc20Balance(hash, proxy)
+		ethereumConfig := config.GetChainListenConfig(basedef.ETHEREUM_CROSSCHAIN_ID)
+		if ethereumConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := ethereumSdk.Erc20Balance(hash, ethereumConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = ethereumConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range ethereumConfig.OtherProxyContract {
+			amount, err := ethereumSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
 	}
-	return new(big.Int).SetUint64(0), nil
+	if chainId == basedef.MATIC_CROSSCHAIN_ID {
+		maticConfig := config.GetChainListenConfig(basedef.MATIC_CROSSCHAIN_ID)
+		if maticConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := maticSdk.Erc20Balance(hash, maticConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = maticConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range maticConfig.OtherProxyContract {
+			amount, err := maticSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.BSC_CROSSCHAIN_ID {
+		bscConfig := config.GetChainListenConfig(basedef.BSC_CROSSCHAIN_ID)
+		if bscConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := bscSdk.Erc20Balance(hash, bscConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = bscConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range bscConfig.OtherProxyContract {
+			amount, err := bscSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.HECO_CROSSCHAIN_ID {
+		hecoConfig := config.GetChainListenConfig(basedef.HECO_CROSSCHAIN_ID)
+		if hecoConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := hecoSdk.Erc20Balance(hash, hecoConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = hecoConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range hecoConfig.OtherProxyContract {
+			amount, err := hecoSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.OK_CROSSCHAIN_ID {
+		okConfig := config.GetChainListenConfig(basedef.OK_CROSSCHAIN_ID)
+		if okConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := okSdk.Erc20Balance(hash, okConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = okConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range okConfig.OtherProxyContract {
+			amount, err := okSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.NEO_CROSSCHAIN_ID {
+		neoConfig := config.GetChainListenConfig(basedef.NEO_CROSSCHAIN_ID)
+		if neoConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := neoSdk.Nep5Balance(hash, neoConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = neoConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range neoConfig.OtherProxyContract {
+			amount, err := neoSdk.Nep5Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.ONT_CROSSCHAIN_ID {
+		ontConfig := config.GetChainListenConfig(basedef.ONT_CROSSCHAIN_ID)
+		if ontConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := ontologySdk.Oep4Balance(hash, ontConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = ontConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range ontConfig.OtherProxyContract {
+			amount, err := ontologySdk.Oep4Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	if chainId == basedef.ARBITRUM_CROSSCHAIN_ID {
+		arbitrumConfig := config.GetChainListenConfig(basedef.ARBITRUM_CROSSCHAIN_ID)
+		if arbitrumConfig == nil {
+			panic("chain is invalid")
+		}
+		amount, err := arbitrumSdk.Erc20Balance(hash, arbitrumConfig.ProxyContract)
+		if err == nil {
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = "poly"
+			proxyBalance.ItemProxy = arbitrumConfig.ProxyContract
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		for _, otherProxyContract := range arbitrumConfig.OtherProxyContract {
+			amount, err := arbitrumSdk.Erc20Balance(hash, otherProxyContract.ItemProxy)
+			if err != nil {
+				continue
+			}
+			proxyBalance := new(ProxyBalance)
+			proxyBalance.Amount = amount
+			proxyBalance.ItemName = otherProxyContract.ItemName
+			proxyBalance.ItemProxy = otherProxyContract.ItemProxy
+			proxyBalances = append(proxyBalances, proxyBalance)
+		}
+		return proxyBalances
+	}
+	return proxyBalances
+}
+
+func GetProxyBalance(chainId uint64, hash string, proxy string) (*big.Int, error) {
+	switch chainId {
+	case basedef.ETHEREUM_CROSSCHAIN_ID:
+		return ethereumSdk.Erc20Balance(hash, proxy)
+	case basedef.MATIC_CROSSCHAIN_ID:
+		return maticSdk.Erc20Balance(hash, proxy)
+	case basedef.BSC_CROSSCHAIN_ID:
+		return bscSdk.Erc20Balance(hash, proxy)
+	case basedef.HECO_CROSSCHAIN_ID:
+		return hecoSdk.Erc20Balance(hash, proxy)
+	case basedef.OK_CROSSCHAIN_ID:
+		return okSdk.Erc20Balance(hash, proxy)
+	case basedef.NEO_CROSSCHAIN_ID:
+		return neoSdk.Nep5Balance(hash, proxy)
+	case basedef.ONT_CROSSCHAIN_ID:
+		return ontologySdk.Oep4Balance(hash, proxy)
+	case basedef.ARBITRUM_CROSSCHAIN_ID:
+		return arbitrumSdk.Erc20Balance(hash, proxy)
+	case basedef.XDAI_CROSSCHAIN_ID:
+		return xdaiSdk.Erc20Balance(hash, proxy)
+
+	default:
+		return new(big.Int).SetUint64(0), nil
+	}
 }
